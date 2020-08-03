bfd2ff435caa622f6cc0561c9c7d0984b0bc713f
Patrick-Manyara <pmanyara97@gmail.com>
2020-08-03 12:10:36 +0300

    Additional UI changes

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,37 +1,20 @@
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
-<<<<<<< HEAD
     window.location.hostname === "[::1]" ||
-=======
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
->>>>>>> 447c6b3a14ca7d01b981103fbaf3b28bd603e502
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
 
 export function register(config) {
-<<<<<<< HEAD
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-=======
-  if (
-    /*process.env.NODE_ENV === "production" && */ "serviceWorker" in navigator
-  ) {
-    // The URL constructor is available in all browsers that support SW.
->>>>>>> 447c6b3a14ca7d01b981103fbaf3b28bd603e502
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
 
     window.addEventListener("load", () => {
-<<<<<<< HEAD
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-=======
-      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
->>>>>>> 447c6b3a14ca7d01b981103fbaf3b28bd603e502
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
@@ -70,12 +53,6 @@ function registerValidSW(swUrl, config) {
                 config.onUpdate(registration);
               }
             } else {
-<<<<<<< HEAD
-=======
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
->>>>>>> 447c6b3a14ca7d01b981103fbaf3b28bd603e502
               console.log("Content is cached for offline use.");
 
               if (config && config.onSuccess) {
@@ -92,31 +69,19 @@ function registerValidSW(swUrl, config) {
 }
 
 function checkValidServiceWorker(swUrl, config) {
-  
   fetch(swUrl)
     .then((response) => {
-<<<<<<< HEAD
-      
-=======
-      // Ensure service worker exists, and that we really are getting a JS file.
->>>>>>> 447c6b3a14ca7d01b981103fbaf3b28bd603e502
       const contentType = response.headers.get("content-type");
       if (
         response.status === 404 ||
         (contentType != null && contentType.indexOf("javascript") === -1)
       ) {
-<<<<<<< HEAD
-       
-=======
-        // No service worker found. Probably a different app. Reload the page.
->>>>>>> 447c6b3a14ca7d01b981103fbaf3b28bd603e502
         navigator.serviceWorker.ready.then((registration) => {
           registration.unregister().then(() => {
             window.location.reload();
           });
         });
       } else {
-        
         registerValidSW(swUrl, config);
       }
     })
--- src/soundAnalyser.js
+++ src/soundAnalyser.js
@@ -6,7 +6,6 @@ class SoundAnalyser extends Component {
     this.state = {
       audio: null,
     };
-<<<<<<< HEAD
   }
 
   // crete the display
@@ -71,86 +70,6 @@ class SoundAnalyser extends Component {
         );
         posX += barWidth + 1;
       }
-=======
-
-    // crete the display
-    createFrequencyBar = () => {
-        const audioCtx = new AudioContext();
-        audioCtx.crossOrigin = "anonymous";//important for CORS
-
-        //Create audio source
-        //Here, we use an audio file, but this could also be e.g. microphone input
-        const audioEle = new Audio();
-        audioEle.crossOrigin = "anonymous";//important for CORS
-
-        audioEle.src = 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p';
-        audioEle.autoplay = true;
-        audioEle.preload = 'auto';
-        const audioSourceNode = audioCtx.createMediaElementSource(audioEle);
-
-        //Create analyser node
-        const analyserNode = audioCtx.createAnalyser();
-        analyserNode.fftSize = 256;
-        const bufferLength = analyserNode.frequencyBinCount;
-        const dataArray = new Float32Array(bufferLength);
-
-        //Set up audio node network
-        audioSourceNode.connect(analyserNode);
-        analyserNode.connect(audioCtx.destination);
-
-        //Create 2D canvas
-        const canvas = document.createElement('canvas');
-        canvas.style.position = 'absolute';
-        canvas.style.top = 0;
-        canvas.style.left = 0;
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        document.body.appendChild(canvas);
-        const canvasCtx = canvas.getContext('2d');
-        canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-        function draw() {
-            //Schedule next redraw
-            requestAnimationFrame(draw);
-
-            //Get spectrum data
-            analyserNode.getFloatFrequencyData(dataArray);
-
-            //Draw black background
-            canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-            canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-            //Draw spectrum
-            const barWidth = (canvas.width / bufferLength) * 2.5;
-            let posX = 0;
-            for (let i = 0; i < bufferLength; i++) {
-                const barHeight = (dataArray[i] + 140) * 2;
-                canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight + 100) + ', 50, 50)';
-                canvasCtx.fillRect(posX, canvas.height - barHeight / 2, barWidth, barHeight / 2);
-                posX += barWidth + 1;
-            }
-        };
-
-        draw();
-    }
-    // end of crete the display
-
-    toogleStates = () => {
-
-        this.setState({ audio: !this.state.audio })
-
-    }
-
-    render() {
-
-        return (
-            <div>
-                <button onClick={this.toogleStates}> {this.state.audio ? "on" : 'off'} </button>
-                {this.state.audio ? this.createFrequencyBar : ''}
-            </div>
-        )
->>>>>>> 447c6b3a14ca7d01b981103fbaf3b28bd603e502
     }
 
     draw();
